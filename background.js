097455417b4967d448c8a8b62f5a0e4b3c0dff93
HJ14 <jeongheejae14@gmail.com>
2025-01-13 01:14:09 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,30 +1,16 @@
-const colors = [
-  '#ef5777',
-  '#575fcf',
-  '#4bcffa',
-  '#34e7e4',
-  '#0be881',
-  '#f53b57',
-  '#3c40c6',
-  '#0fbcf9',
-  '#00d8d6',
-  '#05c46b',
-  '#ffc048',
-  '#ffdd59',
-  '#ff5e57',
-  '#d2dae2',
-  '#485460',
-  '#ffa801',
-  '#ffd32a',
-  '#ff3f34',
+const images = [
+  '1.jpg',
+  '2.jpg',
+  '3.jpg',
 ];
 
 const body = document.querySelector('body');
 
 function paintColor() {
-  const color1 = colors[Math.floor(Math.random() * colors.length)];
-  const color2 = colors[Math.floor(Math.random() * colors.length)];
-  body.style.background = `linear-gradient(${color1},${color2})`;
+  const chosenImage = images[Math.floor(Math.random() * images.length)];
+  body.style.background = `url(${chosenImage})`;
 }
 
 paintColor();
+
+
